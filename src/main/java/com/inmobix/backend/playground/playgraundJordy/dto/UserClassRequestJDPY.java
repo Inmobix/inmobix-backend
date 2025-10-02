a1cef587e4751bbc282b34b248d743aeaa80b4bc
JordyPradaYanes <jdpraday@ufpso.edu.co>
2025-10-02 00:37:02 -0500

    INB-14 Crear entidad y repositorio PropertyClass

--- src/main/java/com/inmobix/backend/playground/playgraundJordy/dto/UserClassRequestJDPY.java
+++ src/main/java/com/inmobix/backend/playground/playgraundJordy/dto/UserClassRequestJDPY.java
@@ -1,13 +1,11 @@
 package com.inmobix.backend.playgraundJordy.dto;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
-public class UserClassResponse {
-    private Long id;
+public class UserClassRequestJDPY {
     private String name;
     private String email;
     private String phone;
+
+
 }
--- src/main/java/com/inmobix/backend/playground/playgraundJordy/repository/UserClassRepository.java
+++
@@ -1,9 +0,0 @@
-package com.inmobix.backend.playgraundJordy.repository;
-
-import com.inmobix.backend.playgraundJordy.model.UserClass;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserClassRepository extends JpaRepository<UserClass, Long> {
-    Optional<UserClass> findByEmail(String email);
-}
